22d06ee0ed14938abb353e04b265ae383c20189a
Catalina Arroyo <151681207+caadf@users.noreply.github.com>
2024-01-08 21:00:20 +0000

    cambios en codigos

--- src/app.js
+++ src/app.js
@@ -5,3 +5,8 @@ import "./style.css";
 window.onload = function() {
   //write your code here
 };
+
+let iconos = ["♦", "♥", "♠", "♣"];
+function iconosaleatorios() {
+  let numero = Math.floor(Math.random() * iconos.lenght);
+}
