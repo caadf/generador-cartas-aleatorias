18a8036539b13fbd870991dbfcb8a877eab1aa3f
Catalina Arroyo <151681207+caadf@users.noreply.github.com>
2024-01-09 17:48:44 +0000

    ejercicio terminado

--- src/app.js
+++ src/app.js
@@ -2,11 +2,47 @@
 import "bootstrap";
 import "./style.css";
 
-window.onload = function() {
-  //write your code here
-};
-
 let iconos = ["♦", "♥", "♠", "♣"];
-function iconosaleatorios() {
-  let numero = Math.floor(Math.random() * iconos.lenght);
+let numeros = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
+
+function iconosAleatorios() {
+  let numero = Math.floor(Math.random() * iconos.length);
+  return numero;
+}
+
+function numerosAleatorios() {
+  let numero = Math.floor(Math.random() * numeros.length);
+  return numero;
+}
+
+let iconoArriba = document.querySelector(".icono-arriba");
+let obtenerNumero = document.querySelector(".numero");
+let iconoAbajo = document.querySelector(".icono-abajo");
+let icono = iconos[iconosAleatorios()];
+let numero = numeros[numerosAleatorios()];
+
+iconoArriba.innerHTML = icono;
+obtenerNumero.innerHTML = numero;
+iconoAbajo.innerHTML = icono;
+
+if (icono == "♦" || icono == "♥") {
+  iconoArriba.style.color = "red";
+  iconoAbajo.style.color = "red";
+} else {
+  iconoArriba.style.color = "black";
+  iconoAbajo.style.color = "black";
 }
